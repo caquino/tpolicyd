597dee3e1fdb6696bbbd842e59f86a266e3f7f09
Alexandre Fiori <fiorix@gmail.com>
2010-01-12 20:55:26 -0200

    geoip back to sqlite

--- tools/geoip_generate.sh
+++ tools/geoip_generate.sh
@@ -2,5 +2,5 @@
 
 wget http://ipinfodb.com/mirror/ip_database/current/ipinfodb_one_table_full_country.csv.zip
 unzip ipinfodb_one_table_full_country.csv.zip
-python geoip_cdbmake.py
+python geoip_make.py
 rm -f ipinfodb_one_table_full_country.csv.zip ip_group_country.csv
