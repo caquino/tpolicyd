97a289d69cda8cd1f787265f373aa4c0909edc53
Alexandre Fiori <fiorix@gmail.com>
2010-01-12 19:48:54 -0200

    changed from sqlite to cdb

--- tools/geoip_generate.sh
+++ tools/geoip_generate.sh
@@ -2,5 +2,5 @@
 
 wget http://ipinfodb.com/mirror/ip_database/current/ipinfodb_one_table_full_country.csv.zip
 unzip ipinfodb_one_table_full_country.csv.zip
-python geoip_import.py
+python geoip_cdbmake.py
 rm -f ipinfodb_one_table_full_country.csv.zip ip_group_country.csv
